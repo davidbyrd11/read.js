56e765601232bc1534b92c1ae97f38fda17c0505
David Byrd <david@byrdhou.se>
2013-03-04 00:58:57 -0500

    just started playing around with stuff. Nothing works yet

--- read.js
+++ read.js
@@ -1,10 +1,21 @@
-(function( $ ) {
-  $.fn.read = function() {
-  
-    this.scroll(function () { // if they reach the bottom let's naively assume they've read the page
-      if(this.scrollTop() + this.height() == this.document.height())
-        this.trigger('read');
+(function ($) {
+
+  var start = +new Date();
+
+  $.fn.read = function () {
+    $(window).scroll(function () {
+
+      var st = $(window).scrollTop(),
+          wh = $(window).height(),
+          dh = document.height;
+
+      if (st + wh == dh) {
+        $(window).trigger('read');
+        var elapsed =  (+new Date() - start) / 1000;
+        console.log('User has been on the page for ' + elapsed + ' seconds.');
+      }
     });
+    
 
   };
-})( jQuery );
+}) (jQuery);
